7a002346f5dfee842bc1d87a1e858ab9ccaa4889
Logical Mechanism <77127298+logicalmechanism@users.noreply.github.com>
2021-03-23 14:59:38 -0700

    Updated comments

--- constant_product.py
+++ constant_product.py
@@ -1,71 +1,110 @@
 """
 p2p automated market maker
+
+N tokens are deposited into a liquidity pool. Two random tokens are
+selected. A random amount of the first token is selected to swap with
+the other token. The market automatically creats a swap amount and
+evolves the reserves to determines the new martket price.
+
+A simple model of a constant product market.
+
+
+Guillermo  Angeris  et  al.
+An  analysis  of  Uniswap  markets. 2019. 
+arXiv: https://arxiv.org/abs/1911.03380
+
 """
 import random
-import itertools
+from random import randint, uniform
+from itertools import combinations
 import matplotlib.pyplot as plt
 
+
 def evolve(Ra, Da, Rb, Db):
+    """
+    Evolve the two reserves involved in the swap.
+    """
     Ra = Ra - Da
     Rb = Rb + Db
     return Ra, Rb
 
-def findsubsets(s, n):
-    return list(itertools.combinations(s, n))
 
 def caluclate_amount(X, a, b, g):
+    """
+    See the simple calculation section.
+    https://ancientkraken.gitbook.io/simple-amm/
+    """
     return -(b*X) / (g*(X-a))
+
 
 def select_token_pair(reserves):
     """
     Select a random pair of tokens from the liquidity pool.
     """
-    tokenA = random.randint(0, nTokens-1)
-    tokenB = random.randint(0, nTokens-1)
+    tokenA = randint(0, nTokens-1)
+    tokenB = randint(0, nTokens-1)
     while tokenB == tokenA:
-        tokenB = random.randint(0, nTokens-1)
+        tokenB = randint(0, nTokens-1)
     return tokenA, tokenB
+
 
 def get_market_prices(tokens, reserves):
     """
     Find all pairs and return a dict of every market price.
     """
-    pairs = findsubsets(tokens, 2)
+    pairs = list(combinations(tokens, 2))
     prices = {}
     for t in tokens:
         prices[t] = {}
     for pair in pairs:
         market_price = reserves[pair[1]]/reserves[pair[0]]
+        # Get price for both directions.
         prices[pair[0]][pair[1]] = market_price
         prices[pair[1]][pair[0]] = 1/market_price
     return prices
 
-def simulation(nTokens, gamma, transactions):
+
+def simulation(nTokens, gamma, transactions,plot=False):
     """
     Run a market simulation of nTokens with a gamma fee for t transactions.
     """
-    reserves = [random.randint(2500000, 10000000) for _ in range(nTokens)]
+    minimum = 2.5*pow(10,7)
+    maximum = 10*pow(10,7)
+    reserves = [randint(minimum, maximum) for _ in range(nTokens)]
     tokens = [i for i in range(nTokens)]
+    print('\nToken Labels {}'.format(tokens))
+    print('Liquidity Pools {}'.format(reserves))
+    print('Transaction Fee {}'.format(1-gamma))
+
     # The number of transactions
-    for _ in range(transactions):
+    for t in range(transactions):
+
+        # Randon select two tokens
         tokenA, tokenB = select_token_pair(reserves)
-        print('\nTrading Token {} for Token {}'.format(tokenA, tokenB))
         reserveA = reserves[tokenA]
         reserveB = reserves[tokenB]
+
         # random select delta A and calculate delta B
-        deltaA = random.uniform(0,reserveA//32)
+        deltaA = uniform(0, 1000)
+        print('\nTrading {} of Token {} for Token {}'.format(deltaA, tokenA, tokenB))
         deltaB = caluclate_amount(deltaA, reserveA, reserveB, gamma)
-        reserveA, reserveB = evolve(reserveA, deltaA, reserveB, deltaB)
+        print('Paying {} of Token {}'.format(deltaB, tokenB))
+
         # Update Reserve
+        reserveA, reserveB = evolve(reserveA, deltaA, reserveB, deltaB)
         reserves[tokenA] = reserveA
         reserves[tokenB] = reserveB
         market_price = get_market_prices(tokens, reserves)
         print('Market Prices After Trade')
         print(market_price)
-        # print(market_price[0][1])
+
+        # Plot if want
+        if plot is True:
+            plt.scatter(t, market_price[0][1], c='k')
+    plt.show()
 
 if __name__ == "__main__":
     gamma = 1 # feeless
-    nTokens = 2
-    transactions = 1000
-    simulation(nTokens, gamma, transactions)
+    nTokens = 3
+    transactions = 3
+    simulation(nTokens, gamma, transactions, plot=False)
